f8870779b758706c48bdaf4568f39d7358f6e710
Yen Ko Romero Lay <yenkosac@gmail.com>
2021-02-03 19:32:38 -0500

    nuevo

---
+++ js/js.js
@@ -0,0 +1,13 @@
+var hermano = prompt("como te llamas");
+var elemento = document.getElementById("borrar")
+
+if (hermano === "jean") {
+    alert("hola pio")
+} else if (hermano === "yami") {
+    alert("hola yami")
+} else {
+    alert("entonces quienes eres?")
+
+}
+
+// alert("hola pio");
